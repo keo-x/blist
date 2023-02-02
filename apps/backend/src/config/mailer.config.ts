ea1f25aa796213b55f33f65f62bbebab7eaccbac
keo-x <42323541+keo-x@users.noreply.github.com>
2023-02-02 12:19:42 +0100

    fix mailer configuration

--- apps/backend/src/config/mailer.config.ts
+++ apps/backend/src/config/mailer.config.ts
@@ -1,6 +1,6 @@
 import {MailerOptions, MailerOptionsFactory} from '@nestjs-modules/mailer'
 import {ConfigService} from '@nestjs/config'
-import {isEmpty} from 'rambda'
+import {isEmpty, isNil} from 'rambda'
 
 export class MailerConfiguration implements MailerOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
@@ -8,17 +8,19 @@ export class MailerConfiguration implements MailerOptionsFactory {
   createMailerOptions(): Promise<MailerOptions> | MailerOptions {
     const defaultPort = 2025
     const isAuthSet =
-      !isEmpty(process.env.MAIL_USERNAME) && !isEmpty(process.env.MAIL_PASSWORD)
+      !isNil(process.env.MAIL_USERNAME) &&
+      !isNil(process.env.MAIL_PASSWORD) &&
+      !isEmpty(process.env.MAIL_USERNAME) &&
+      !isEmpty(process.env.MAIL_PASSWORD)
 
     return {
       transport: {
         host: process.env.MAIL_HOST,
-        port: process.env.MAIL_HOST
-          ? parseInt(process.env.MAIL_HOST, 10)
+        port: process.env.MAIL_PORT
+          ? parseInt(process.env.MAIL_PORT, 10)
           : defaultPort,
         ignoreTLS: true,
         secure: false,
-        from: process.env.DEFAULT_EMAIL,
         auth: isAuthSet
           ? {
               user: process.env.MAIL_USERNAME,
