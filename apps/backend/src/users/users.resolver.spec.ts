a8def58d8db5d955305fcf26625ef7a02e23c40a
keo-x <42323541+keo-x@users.noreply.github.com>
2023-02-07 05:00:50 +0100

    fix inport in tests

--- apps/backend/src/users/users.resolver.spec.ts
+++ apps/backend/src/users/users.resolver.spec.ts
@@ -1,13 +1,13 @@
 import {Test, TestingModule} from '@nestjs/testing'
 import {UsersResolver} from './users.resolver'
-import {UsersService} from './users.service'
+import {UserService} from './users.service'
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver, UsersService],
+      providers: [UsersResolver, UserService],
     }).compile()
 
     resolver = module.get<UsersResolver>(UsersResolver)
