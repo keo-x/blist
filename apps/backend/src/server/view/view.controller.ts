96b06ee5888cc66c77fe86ceb95ffe489ea535cd
keo-x <42323541+keo-x@users.noreply.github.com>
2023-02-12 08:24:21 +0100

    add add initial client page

--- apps/backend/src/server/view/view.controller.ts
+++ apps/backend/src/server/view/view.controller.ts
@@ -1,4 +1,17 @@
-import {Controller} from '@nestjs/common'
+import {Controller, Get, Res, Req} from '@nestjs/common'
+import {Request, Response} from 'express'
+import {Public} from '../app/common/decorators'
 
-@Controller('view')
-export class ViewController {}
+import {ViewService} from './view.service'
+
+@Controller('/')
+export class ViewController {
+  constructor(private viewService: ViewService) {}
+
+  @Public()
+  @Get('*')
+  static(@Req() req: Request, @Res() res: Response) {
+    const handle = this.viewService.getNextServer().getRequestHandler()
+    handle(req, res)
+  }
+}
--- apps/backend/src/server/view/view.service.ts
+++ apps/backend/src/server/view/view.service.ts
@@ -1,4 +1,29 @@
-import {Injectable} from '@nestjs/common'
+import {Injectable, OnModuleInit} from '@nestjs/common'
+import {ConfigService} from '@nestjs/config'
+import {dir} from 'console'
+import next from 'next'
+import {NextServer} from 'next/dist/server/next'
+import {join} from 'path'
 
 @Injectable()
-export class ViewService {}
+export class ViewService implements OnModuleInit {
+  private server!: NextServer
+
+  constructor(private configService: ConfigService) {}
+
+  async onModuleInit(): Promise<void> {
+    try {
+      this.server = next({
+        dev: this.configService.get<string>('NODE_ENV') !== 'production',
+        dir: './apps/backend/src/client',
+      })
+      await this.server.prepare()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  getNextServer(): NextServer {
+    return this.server
+  }
+}
